a662c784e11fd4668bb8c0b4c242ee392f819b80
Pierre Payet <pierre.payet@outlook.com>
2025-01-03 11:12:17 +0100

    feat: add Gilded Rose kata linkedin post

--- app/data/linkedin-posts.ts
+++ app/data/linkedin-posts.ts
@@ -3,6 +3,10 @@ const linkedinPosts: Array<{
   title: string;
 }> = [
   {
+    href: 'https://www.linkedin.com/posts/pierre-payet-457476136_gilded-rose-refactoring-kata-typescript-activity-7277956346795229185-ED6Q',
+    title: "Publication de l'article sur le kata Gilded Rose.",
+  },
+  {
     href: 'https://www.linkedin.com/posts/pierre-payet-457476136_document-corrector-correcteur-de-documents-activity-7216358008556060672--1ip',
     title: 'Présentation de Document Corrector.',
   },
