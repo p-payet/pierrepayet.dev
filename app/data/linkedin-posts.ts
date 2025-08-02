765d98d57c5069e9b815456dccdcecfd47af94f6
Pierre Payet <pierre.payet@outlook.com>
2025-08-02 13:54:47 +0200

    feat(data/linkedin-posts): remove old LinkedIn post, and add new one

--- app/data/linkedin-posts.ts
+++ app/data/linkedin-posts.ts
@@ -3,12 +3,12 @@ const linkedinPosts: Array<{
   title: string;
 }> = [
   {
-    href: 'https://www.linkedin.com/posts/pierre-payet-457476136_gilded-rose-refactoring-kata-typescript-activity-7277956346795229185-ED6Q',
-    title: "Publication de l'article sur le kata Gilded Rose.",
+    href: 'https://www.linkedin.com/posts/pierre-payet-457476136_merci-%C3%A0-malt-pour-cette-belle-attention-activity-7345722948630028288-Lt_q',
+    title: 'De "Super Malter" à une nouvelle aventure chez Inicio.',
   },
   {
-    href: 'https://www.linkedin.com/posts/pierre-payet-457476136_document-corrector-correcteur-de-documents-activity-7216358008556060672--1ip',
-    title: 'Présentation de Document Corrector.',
+    href: 'https://www.linkedin.com/posts/pierre-payet-457476136_gilded-rose-refactoring-kata-typescript-activity-7277956346795229185-ED6Q',
+    title: "Publication de l'article sur le kata Gilded Rose.",
   },
 ];
 
