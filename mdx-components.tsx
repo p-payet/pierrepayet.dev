8a7fe660ec2401ab89b6e09e95383b35095f1fc6
Pierre Payet <pierre.payet@outlook.com>
2025-12-26 15:05:02 +0100

    Revert "fix: use custom img component with lazy loading to prevent loading warnings for MDX component (blog post)"
    This reverts commit 68e330978b5781e8f06a4af261228805e20d6e52.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,12 +2,6 @@ import type { MDXRemoteProps } from 'next-mdx-remote/rsc';
 
 type MDXComponents = MDXRemoteProps['components'];
 
-// Custom img component with lazy loading to prevent preload warnings
-function MDXImage({ src, alt, ...props }: { src?: string; alt?: string;[key: string]: unknown }) {
-  // eslint-disable-next-line @next/next/no-img-element
-  return <img loading="lazy" src={src} alt={alt ?? ''} {...props} />;
-}
-
 // This file allows you to provide custom React components
 // to be used in MDX files. You can import and use any
 // React component you want, including components from
@@ -18,7 +12,8 @@ export function useMDXComponents(
   components: MDXComponents
 ): MDXComponents {
   return {
-    img: MDXImage,
+    // Allows customizing built-in components, e.g. to add styling.
+    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
     ...components,
   };
 }
